594e4035b9d536fe8299abda7def7710fea82204
Vincent Bouzeran <vincent.bouzeran@elao.com>
2014-10-20 17:27:21 +0200

    Add default parameters to config.
    Only the source property is required now.

--- configuration/loader.js
+++ configuration/loader.js
@@ -4,12 +4,35 @@ var path = require('path');
 
 var cfg    = __dirname + '/../config.js';
 var loaded = false;
+var defaultConfig = {
+    port:           5555,
+    store:          'memory',
+    debug:          false,
+    outputFolder:   __dirname + "/../content",
+    backupFile:     __dirname + "/../current.json",
+    assetsToCopy:   [{
+        from: __dirname + "/../content/posts/images/",
+        to:   __dirname + "/../public/images",
+        url:  "/images"
+    }],
+    security: {
+        rules: {
+            guest: {},
+            admin: {}
+        },
+        basic: {
+            realm:    'BlogD Private Api',
+            user:     'admin',
+            password: 'private'
+        }
+    }
+}
 
 
 if (fs.existsSync(cfg)) {
     console.info("Loading config from " + path.resolve(cfg));
     loaded = true;
-    module.exports = require('../config.js');
+    module.exports = _.defaults(require('../config.js'), defaultConfig);
 } else {
     var cnt = 0;
     var p1f = path.resolve(__dirname + '/../package.json');
@@ -19,7 +42,7 @@ if (fs.existsSync(cfg)) {
         var pkg = require(p1f);
         if (_.has(pkg, 'blogd')) {
             console.info("Loading config from " + p1f);
-            module.exports = pkg.blogd;
+            module.exports = _.defaults(pkg.blogd, defaultConfig);
             loaded = true;
         }
     }
@@ -28,7 +51,7 @@ if (fs.existsSync(cfg)) {
         var pkg = require(p2f);
         if (_.has(pkg, 'blogd')) {
             console.info("Loading config from " + p2f);
-            module.exports = pkg.blogd;
+            module.exports = _.defaults(pkg.blogd, defaultConfig);
             loaded = true;
         }
     }
